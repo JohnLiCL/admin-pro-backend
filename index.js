9f38ee67b882ba2a46e3af8bca7b76de266b42eb
John Suárez <john.suarez.devs@gmail.com>
2022-10-06 18:19:39 -0300

    SPA Control

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 
 const { dbConnection } = require('./database/config');
@@ -21,12 +21,16 @@ app.use(express.json());
 dbConnection();
 
 //Rutas
-app.use('/api/usuarios',require('./routes/usuarios'));
-app.use('/api/hospitales',require('./routes/hospitales'));
-app.use('/api/medicos',require('./routes/medicos'));
-app.use('/api/login',require('./routes/auth'));
-app.use('/api/busqueda',require('./routes/busquedas'));
-app.use('/api/upload',require('./routes/uploads'));
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/hospitales', require('./routes/hospitales'));
+app.use('/api/medicos', require('./routes/medicos'));
+app.use('/api/login', require('./routes/auth'));
+app.use('/api/busqueda', require('./routes/busquedas'));
+app.use('/api/upload', require('./routes/uploads'));
+
+app.get('*', (req, res) => {
+  res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
+});
 
 app.listen(process.env.PORT, () => {
   console.log('Servicor Corriendo en el puerto: ', process.env.PORT);
